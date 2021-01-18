67928b7583a72f6ca76b054e5802cafc24eb463c
alirezaatabaki <alirezaatabaki@icloud.com>
2021-01-18 12:09:17 +0330

    6

--- echo/urls.py
+++ echo/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from echo import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('image/', views.echo_image, name='echo_image'),
 ]
--- echo/views.py
+++ echo/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'echo/index.html')
+
+
+def echo_image(request):
+    return render(request, 'echo/echo_image.html')
